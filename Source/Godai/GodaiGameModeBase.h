b958a07a7d277ae3e2ba65d8eba7be20f9084ee8
Justin Camden <justin@geistnet.com>
2020-10-22 19:45:05 -0700

    Initalized project

---
+++ Source/Godai/GodaiGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "GodaiGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GODAI_API AGodaiGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
